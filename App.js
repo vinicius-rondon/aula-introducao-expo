7fd64d5e9cd3cc2616ebfda5042c1d5ed64843ac
vinicius-rondon <viniciusrondon01@gmail.com>
2026-05-12 19:27:14 -0300

    commit do dia 12 do 5

--- App.js
+++ App.js
@@ -2,13 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 
 //import Exemplo01 from './src/exemplos/ex01';
+import Exemplo05 from './src/exemplos/ex05';
+import Exemplo06 from './src/exemplos/ex06';
+
 
 //import Att01 from './src/atividades/Att01';
-
 //import Att02 from './src/atividades/att-02';
 import Att03 from './src/atividades/Att03';
 import Att04 from './src/atividades/Att04'
 import Att05 from './src/atividades/Att05'
+
 
 //para importar os exemplos escreva exatamente como esta no arquivo q esta em sua respectiva pasta 
 //nao esquece de mudar embaixo do view
